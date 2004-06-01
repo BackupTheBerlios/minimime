e21d63df0c18959445d13f324e1b9d1571cdac7e
jfi <jfi>
2004-06-01 06:51:31 +0000

    can now test both, file and memory based parser copyright/license header

--- minimime/src/minimime.c
+++ minimime/src/minimime.c
@@ -1,11 +1,56 @@
 /*
+ * Copyright (c) 2004 Jann Fischer. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
  * MiniMIME test program
  */
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <getopt.h>
 
 #include "mm.h"
+
+void
+usage(void)
+{
+	fprintf(stderr,
+	"MiniMIME test suite\n"
+	"Usage: ./minimime [-m] <filename>\n\n"
+	"   -m            : use memory based scanning\n\n"
+	);
+	exit(1);
+}
 
 int
 main(int argc, char **argv)
@@ -16,12 +61,28 @@ main(int argc, char **argv)
 	struct mm_mimepart *part;
 	struct mm_content *ct;
 	int parts, i;
+	struct stat st;
+	int fd;
+	char *buf;
+	int scan_mode = 0;
 
 	lastheader = NULL;
 
-	if (argc < 2) {
-		printf("USAGE: %s <filename>\n", argv[0]);
-		exit(1);
+	while ((i = getopt(argc, argv, "m")) != -1) {
+		switch(i) {
+		case 'm':
+			scan_mode = 1;
+			break;
+		default:
+			usage();
+		}
+	}
+
+	argc -= optind;
+	argv += optind;
+
+	if (argc < 1) {
+		usage();
 	}
 	
 #ifdef __HAVE_LEAK_DETECTION
@@ -39,8 +100,34 @@ main(int argc, char **argv)
 		/* Create a new context */
 		ctx = mm_context_new();
 
+
 		/* Parse a file into our context */
-		if (mm_parse_file(ctx, argv[1], MM_PARSE_LOOSE, 0) == -1) {
+		if (scan_mode == 0) {
+			i = mm_parse_file(ctx, argv[0], MM_PARSE_LOOSE, 0);
+		} else {
+			if (stat(argv[0], &st) == -1) {
+				err(1, "stat");
+			}
+	
+			if ((fd = open(argv[0], O_RDONLY)) == -1) {
+				err(1, "open");
+			}
+
+			buf = (char *)malloc(st.st_size);
+			if (buf == NULL) {
+				err(1, "malloc");
+			}	
+
+			if (read(fd, buf, st.st_size) != st.st_size) {
+				err(1, "read");
+			}
+
+			close(fd);
+			buf[st.st_size] = '\0';
+			
+			i = mm_parse_mem(ctx, buf, MM_PARSE_LOOSE, 0);
+		}
+		if (i == -1) {	
 			printf("ERROR: %s\n", mm_error_string());
 			exit(1);
 		}
