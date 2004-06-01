8c1ccb6067e31cb71f930de99e82e3f156fef96c
jfi <jfi>
2004-06-01 07:22:16 +0000

    use memory based buffers for tests, too

--- minimime/src/test.sh
+++ minimime/src/test.sh
@@ -10,13 +10,20 @@ LD_LIBRARY_PATH=.
 export LD_LIBRARY_PATH
 
 for f in test/test*.txt; do
-	echo -n "Running test for $f... "
+	echo -n "Running test for $f (file)... "
 	output=`./minimime $f 2>&1`
 	[ $? != 0 ] && {
 		echo "FAILED ($output)"
 	} || {
 		echo "PASSED"
 	}
+	echo -n "Running test for $f (memory)... "
+	output=`./minimime -m $f 2>&1`
+	[ $? != 0 ] && {
+		echo "FAILED ($output)"
+	} || {
+		echo "PASSED"
+	}	
 done
 
 unset LD_LIBRARY_PATH
