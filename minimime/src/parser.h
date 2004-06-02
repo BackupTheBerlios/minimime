57c5b72b8f2ddeb116b5f2ddcda8a60a421c86b9
jfi <jfi>
2004-06-02 01:05:07 +0000

    ok, this couldn't work... duh.

---
+++ minimime/src/parser.h
@@ -0,0 +1,21 @@
+#ifndef PARSER_H_INCLUDED
+#define PARSER_H_INCLUDED
+
+/**
+ * Prototypes for functions used by the parser routines
+ */
+int 	count_lines(char *);
+void	mmyyerror(const char *);
+int 	dprintf(const char *, ...);
+int 	mm_yyparse(void);
+int 	mm_yylex(void);
+void	mm_yyerror(const char *);
+
+struct s_position
+{
+	size_t opaque_start;
+	size_t start;
+	size_t end;
+};
+
+#endif /* ! PARSER_H_INCLUDED */
